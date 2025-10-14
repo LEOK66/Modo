7a62706fa1381fe572e000a9c23c136ec230067a
zihaoli <lizihao@cs.washington.edu>
2025-10-14 13:45:52 -0700

    Initial Commit

---
+++ Modo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Modo
+//
+//  Created by 李梓豪 on 2025/10/14.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Modo/ModoApp.swift
@@ -0,0 +1,32 @@
+//
+//  ModoApp.swift
+//  Modo
+//
+//  Created by 李梓豪 on 2025/10/14.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ModoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
