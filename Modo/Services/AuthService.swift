615999d6cb45d1a7895c2e99b81eb4e8b8d32777
Shelby <shelb2@uw.edu>
2025-10-20 14:31:55 -0700

    Updating AuthService implementation for sign up, sign in, sign out.

--- Modo/Services/AuthService.swift
+++ Modo/Services/AuthService.swift
@@ -1,1 +1,38 @@
+import FirebaseAuth
 
+final class AuthService {
+    static let shared = AuthService()
+    private init() {}
+
+    // MARK: - Create Account
+    func signUp(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let user = result?.user {
+                completion(.success(user))
+            }
+        }
+    }
+
+    // MARK: - Sign In
+    func signIn(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let user = result?.user {
+                completion(.success(user))
+            }
+        }
+    }
+
+    // MARK: - Sign Out
+    func signOut() throws {
+        try Auth.auth().signOut()
+    }
+
+    // MARK: - Check Auth State
+    func getCurrentUser() -> User? {
+        return Auth.auth().currentUser
+    }
+}
